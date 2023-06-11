d02afa373d80a854b7794bd9b5c29e8ee1410fe7
Muriel Carvalho <muriel.carvalho@elotech.com.br>
2023-06-11 18:34:32 -0300

    Subindo busca da pauta

--- src/main/java/br/com/dbc/votacao/controllers/PautaController.java
+++ src/main/java/br/com/dbc/votacao/controllers/PautaController.java
@@ -1,10 +1,16 @@
 package br.com.dbc.votacao.controllers;
 
 import br.com.dbc.votacao.dtos.PautaDto;
+import br.com.dbc.votacao.models.Pauta;
 import br.com.dbc.votacao.services.PautaService;
+import br.com.dbc.votacao.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,5 +29,13 @@ public class PautaController {
     public ResponseEntity<Object> cadastrarPauta(@RequestBody @Validated(PautaDto.PautaView.RegistroPauta.class)
                                                  @JsonView(PautaDto.PautaView.RegistroPauta.class) PautaDto pautaDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(pautaService.cadastrarPauta(pautaDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Pauta>> buscarTodasAsPautas(SpecificationTemplate.PautaSpec spec,
+                                                           @PageableDefault(page = 0, size = 10, sort = "fullName", direction = Sort.Direction.ASC)
+                                                           Pageable pageable){
+        Page<Pauta> pautas = pautaService.buscarTodasAsPautas(spec, pageable);
+        return ResponseEntity.ok()
     }
 }
--- src/main/java/br/com/dbc/votacao/repositories/PautaRepository.java
+++ src/main/java/br/com/dbc/votacao/repositories/PautaRepository.java
@@ -2,6 +2,7 @@ package br.com.dbc.votacao.repositories;
 
 import br.com.dbc.votacao.models.Pauta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PautaRepository extends JpaRepository<Pauta, Long> {
+public interface PautaRepository extends JpaRepository<Pauta, Long>, JpaSpecificationExecutor<Pauta> {
 }
